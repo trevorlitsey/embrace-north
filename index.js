a495035a82fff5af61ee85005bfc79972a731c28
Trevor Litsey <trevor@calllight.health>
2025-02-20 08:30:31 -0600

    hell ya

--- index.js
+++ index.js
@@ -1,58 +1,80 @@
+require("dotenv").config();
 const puppeteer = require("puppeteer");
 
-// https://embracenorth.marianatek.com/auth/login
+const DATE = 20;
+const INTERVAL = 30;
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false }); // Set to false to see the browser
+const findOpenTime = async () => {
+  console.log("----------------");
+  console.log(`> ${new Date()}`);
+  console.log(`> checking for open times`);
+
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("https://embracenorth.com/schedule"); // Replace with your URL
 
-  try {
-    await page.waitForSelector('a[aria-label="Close"]');
-    await page.$('a[aria-label="Close"]').then((el) => {
-      el.click();
-    });
+  await page.goto(
+    "https://embracenorth.marianaiframes.com/iframe/schedule/daily/48541"
+  );
 
-    // Wait for the iframe to load
-    await page.waitForSelector("iframe");
+  await page.waitForNetworkIdle();
 
-    // Select the iframe
-    const iframeElement = await page.$("iframe");
-    const iframe = await iframeElement.contentFrame();
+  // await page.click(`button[data-test-date-button="${DATE}"]`);
 
-    if (iframe) {
-      // Wait for an element inside the iframe
-      await iframe.waitForSelector("table"); // Replace with the actual selector
-      await iframe.$("table").then(async (el) => {
-        const openLinks = await el.evaluate(() => {
-          const trsWithOpenTimes = [...document.querySelectorAll("tr")].filter(
-            (tr) =>
-              [...tr.querySelectorAll("td")].some((td) =>
-                td.textContent.includes("Reserve")
-              )
-          );
+  await page.waitForSelector(`button[data-test-button="accept-all-cookies"]`);
 
-          return trsWithOpenTimes.map((tr) => {
-            return [
-              tr
-                .querySelector("td")
-                .textContent.replace("60 min.Minneapolis", ""),
-              tr.querySelector("td a").href,
-            ];
-          });
-        });
+  await page.click('button[data-test-button="accept-all-cookies"]');
 
-        console.log(openLinks);
-      });
-      // Click the button inside the iframe
-      //   await iframe.click("button#reserve");
-      //   console.log("Clicked button inside iframe");
-    } else {
-      console.log("Iframe not found");
-    }
-  } catch (error) {
-    console.error("Error interacting with iframe:", error);
+  const table = await page.$("table");
+
+  const openLinks = await table.evaluate(() => {
+    const TIMES = ["6:45 PM", "7:00 PM", "7:15 PM"];
+
+    const trsWithOpenTimes = [...document.querySelectorAll("tr")].filter((tr) =>
+      [...tr.querySelectorAll("td")].some((td) =>
+        td.textContent.includes("Reserve")
+      )
+    );
+
+    return trsWithOpenTimes
+      .map((tr) => {
+        return [
+          tr.querySelector("td").textContent.replace("60 min.Minneapolis", ""),
+          tr.querySelector("td a").href,
+        ];
+      })
+      .filter(([time]) => TIMES.includes(time));
+  });
+
+  if (openLinks.length === 0) {
+    console.log(`> no open times found. next check in ${INTERVAL} minutes`);
+    return;
+  } else {
+    console.log(`> open times found: ${openLinks.map((l) => l[0]).join(", ")}`);
   }
 
+  await page.goto(openLinks[0][1]);
+
+  // login
+  await page.waitForSelector('button[data-test-button="log-in"]');
+  await page.click('button[data-test-button="log-in"]');
+  await page.waitForSelector('input[name="username"]');
+  await page.type('input[name="username"]', process.env.EMAIL);
+  await page.type('input[name="password"]', process.env.PASSWORD);
+  await page.click('button[type="submit"]');
+
+  // confirm
+
+  await page.waitForSelector('button[data-test-button="reserve"]');
+
+  await page.click('button[data-test-button="reserve"]');
+
+  await page.waitForNetworkIdle();
+
   await browser.close();
-})();
+
+  throw new Error("Booked class: ", openLinks[0][0]);
+};
+
+findOpenTime();
+
+setInterval(findOpenTime, 1000 * 60 * 30);
