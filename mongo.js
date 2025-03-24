7b2e73730a6993694c934f9d914b607c5ac81dff
Trevor Litsey <trevor@calllight.health>
2025-03-24 11:28:05 -0500

    add a big try/catch

--- mongo.js
+++ mongo.js
@@ -7,47 +7,53 @@ const { makeReservation, findOpenTime } = require("./embrace");
 (async () => {
   const conn = await connectDB();
 
-  const appointments = await Appointment.find({
-    timeFulfilled: null,
-    times: {
-      $gte: new Date(),
-    },
-  });
+  try {
+    const appointments = await Appointment.find({
+      timeFulfilled: null,
+      times: {
+        $gte: new Date(),
+      },
+    });
 
-  console.log(`> ${appointments.length} pending appointment request(s) found`);
+    console.log(
+      `> ${appointments.length} pending appointment request(s) found`
+    );
 
-  for (let appointment of appointments) {
-    try {
-      const [classId, timeToBook] = await findOpenTime(appointment.times);
+    for (let appointment of appointments) {
+      try {
+        const [classId, timeToBook] = await findOpenTime(appointment.times);
 
-      if (classId) {
-        const user = await User.findById({ _id: appointment.userId });
+        if (classId) {
+          const user = await User.findById({ _id: appointment.userId });
 
-        await makeReservation(
-          classId,
-          user.username,
-          user.getDecryptedPassword()
+          await makeReservation(
+            classId,
+            user.username,
+            user.getDecryptedPassword()
+          );
+
+          appointment.timeFulfilled = timeToBook;
+          appointment.classIdFulfilled = classId;
+
+          await appointment.save();
+        }
+      } catch (e) {
+        console.error(e);
+        console.error(
+          `> Error attempting to book appointment ${appointment._id}`
         );
-
-        appointment.timeFulfilled = timeToBook;
-        appointment.classIdFulfilled = classId;
-
-        await appointment.save();
+        if (appointment.pollingErrors) {
+          appointment.pollingErrors.push(e.message);
+        } else {
+          appointment.pollingErrors = [e.message];
+        }
       }
-    } catch (e) {
-      console.error(e);
-      console.error(
-        `> Error attempting to book appointment ${appointment._id}`
-      );
-      if (appointment.pollingErrors) {
-        appointment.pollingErrors.push(e.message);
-      } else {
-        appointment.pollingErrors = [e.message];
-      }
+
+      appointment.lastChecked = new Date();
+      await appointment.save();
     }
-
-    appointment.lastChecked = new Date();
-    await appointment.save();
+  } catch (e) {
+    console.error(e);
   }
 
   conn.disconnect();
